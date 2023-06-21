345e25b63525a7bbc2b82bce3ceedde557b01005
zmmevd <umiiik17@gmail.com>
2023-06-21 15:55:58 +0300

    Второе задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,50 +2,50 @@
 
 //КВАДРАТНОЕ УРАВНЕНИЕ
 function solveEquation(a, b, c) {
-  let arr = [];
-  let Discriminant = b * b - 4 * a * c;
-  console.log ("D = " + Discriminant);
+	let arr = [];
+	let Discriminant = b * b - 4 * a * c;
+	console.log("D = " + Discriminant);
 
-if (Discriminant < 0 || a == 0) {
-  console.log ("Нет действующих корней");
-  return (arr);
+	if (Discriminant < 0 || a == 0) {
+		console.log("Нет действующих корней");
+		return (arr);
 
-} else if (Discriminant == 0 && a !=0) {
-  let x = - (b / (2 * a));
-  console.log ("Один корень: " + "х" + " =" + x);
-  arr.push(x);
-  return (arr);
+	} else if (Discriminant == 0 && a != 0) {
+		let x = -(b / (2 * a));
+		console.log("Один корень: " + "х" + " =" + x);
+		arr.push(x);
+		return (arr);
 
-} else if (Discriminant > 0 && a !==0) {
-  let x1 = (-b + Math.sqrt(Discriminant) )/(2*a);
-  let x2 = (-b - Math.sqrt(Discriminant) )/(2*a);
-  console.log ("Два корня: " + "х1" + " =" + x1 + "; " + "х2" + " =" + x2 + ";");
-  arr.push(x1, x2);
-  return (arr);
+	} else if (Discriminant > 0 && a !== 0) {
+		let x1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
+		let x2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
+		console.log("Два корня: " + "х1" + " =" + x1 + "; " + "х2" + " =" + x2 + ";");
+		arr.push(x1, x2);
+		return (arr);
 
- } else {
-  console.log ("Введите другие значения");
-};
-  return arr;
+	} else {
+		console.log("Введите другие значения");
+	};
+	return arr;
 };
 
 //ИПОТЕКА
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
- // percent - процент
- // contribution - сумма первоначального взноса
- // amount - сумма кредита
- // countMonths -  длительност в месяцах
+	// percent - процент
+	// contribution - сумма первоначального взноса
+	// amount - сумма кредита
+	// countMonths -  длительност в месяцах
 
- percent = percent / 100; //перевод процента в доли
- let MonthPercent = percent / 12; //расчет процента в мемяц
- let BodyCredit = amount - contribution; //тело кредита
- let PayMonth = BodyCredit * (MonthPercent + (MonthPercent / (((1 + MonthPercent) ** countMonths) - 1))) // месячный платеж
- console.log ("Ежемесячный платеж: " + PayMonth);
+	percent = percent / 100; //перевод процента в доли
+	let MonthPercent = percent / 12; //расчет процента в мемяц
+	let BodyCredit = amount - contribution; //тело кредита
+	let PayMonth = BodyCredit * (MonthPercent + (MonthPercent / (((1 + MonthPercent) ** countMonths) - 1))) // месячный платеж
+	console.log("Ежемесячный платеж: " + PayMonth);
 
- let total = PayMonth * countMonths; //Всего
- total = Math.round(total * 100) / 100; // Округление до 2 знаков после запятой
+	let total = PayMonth * countMonths; //Всего
+	total = Math.round(total * 100) / 100; // Округление до 2 знаков после запятой
 
- console.log("Всего: " + total);
- return total;
+	console.log("Всего: " + total);
+	return total;
 };
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,107 @@
-function getArrayParams(...arr) {
+//Задача 1
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(...arr) {
+	let min;
+	let max;
+	let sum;
+	let avg;
+
+	for (let i = 0; i < arr.length; i++) {
+		min = Math.min(...arr);
+		max = Math.max(...arr);
+		sum = arr.reduce((prev, item) => {
+			return prev + item
+		}, 0);
+		avg = sum / arr.length;
+		avg = +(avg.toFixed(2));
+		//console.log(min, max, sum, avg);
+	};
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
+//getArrayParams(1, 2, 3, 4);
+
+//Задача 2
+
 
 function summElementsWorker(...arr) {
-
-}
+	let sum;
+	if (arr.length === 0) {
+		return 0;
+	};
+	for (let i = 0; i < arr.length; i++) {
+		sum = arr.reduce((prev, item) => {
+			return prev + item
+		}, 0);
+		//console.log("Сумма: " + sum);
+	};
+	return sum;
+};
 
 function differenceMaxMinWorker(...arr) {
-
-}
+  let min;
+  let max;
+  if (arr.length === 0) {
+	return 0;
+};
+  for (let i = 0; i < arr.length; i++) {
+		min = Math.min(...arr);
+		max = Math.max(...arr);
+		//console.log("Разница: " + (max - min));
+	};
+  return (max - min);
+};
 
 function differenceEvenOddWorker(...arr) {
-
-}
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  if (arr.length === 0) {
+	return 0;
+};
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      sumEvenElement = sumEvenElement + arr[i];
+    } else {
+      sumOddElement = sumOddElement + arr[i];
+    };
+};
+	//console.log("Четные: " + sumEvenElement, "Нечетные: " + sumOddElement);
+    //console.log("Разность: " + (sumEvenElement - sumOddElement));
+  return (sumEvenElement - sumOddElement);
+};
+//differenceEvenOddWorker (1, 1, 1, 4)
 
 function averageEvenElementsWorker(...arr) {
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+	if (arr.length === 0) {
+		return 0;
+	};
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			sumEvenElement = sumEvenElement + arr[i];
+			countEvenElement = countEvenElement + 1;
+		} else {
+			continue;
+		};
+	};
+	return (sumEvenElement / countEvenElement);
+};
+//console.log(averageEvenElementsWorker (1, 2, 3, 4));
 
-}
 
-function makeWork (arrOfArr, func) {
+// Задача 3
 
-}
+function makeWork(arrOfArr, func) {
+	let maxWorkerResult = -Infinity;
+	for (let i = 0; i < arrOfArr.length; i++) {
+		const result = func(...arrOfArr[i]);
+		if (result > maxWorkerResult) {
+			maxWorkerResult = result;
+		};
+	};
+	return maxWorkerResult;
+};
